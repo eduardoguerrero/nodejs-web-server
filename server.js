be80947d48a3dc9d7ae716fc8efff0f25fe7f7b5
rene.escobar03 <rene.escobar03@telusinternational.com>
2019-01-30 11:27:29 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,89 @@
+
+
+
+ 
+// nodemon server.js -e js,hbs
+
+
+const express = require('express');
+
+const hbs = require('hbs');
+
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+
+
+//register middleware
+app.use((req, res, next)=>{
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	fs.appendFile('server.log', log + '\n', (error)=>{
+		if(error){
+			console.log('Unable to append to server log');
+		}
+		next();
+	});
+});
+
+//app.use((req, res, next)=>{
+//	res.render('maintenance.hbs');
+//});
+
+app.use(express.static(__dirname + '/public'));
+
+//Helper example
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear();
+});
+
+//Helper example with parameters
+hbs.registerHelper('screamIt', (text)=>{
+	return text.toUpperCase();
+});
+
+
+app.get('/', (req, res)=>{
+	res.render('home.hbs',{
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to my website',
+		//currentYear: new Date().getFullYear()
+	})
+	//res.send('<h1>Hello express</h1>');
+	//res.send({
+		//name: 'Rene',
+		//colors: [
+	//		'red',
+	//		'white'
+	//	]
+	//});
+});
+
+app.get('/about', (req, res)=>{
+	res.render('about.hbs', {
+		pageTitle: 'About Pages',
+		//currentYear: new Date().getFullYear()
+	});
+	//res.send('About Page');
+});
+
+app.get('/bad', (req, res)=>{
+	res.send({
+		errorMessage: 'Unable to handle request.'
+	});
+});
+
+
+app.listen(3000,()=>{
+	console.log('Server is up on port 3000');
+});
+
+
+//templating engine - express js template
+//handlebarsjs
+//npm install hbs
